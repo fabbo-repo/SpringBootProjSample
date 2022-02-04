40106fe3b73bc4df6ff455b89d08ea60ac76b7ec
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2022-02-04 15:13:16 +0100

    Project Created

---
+++ src/main/java/com/proy/SpringBootProySampleApplication.java
@@ -0,0 +1,13 @@
+package com.proy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proy/SpringBootProySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
